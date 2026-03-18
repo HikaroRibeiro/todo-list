5aeeef959e0e21c9179997b6d80dd0ce28ea489e
HikaroRibeiro <hhakawama@gmail.com>
2026-03-17 21:29:20 -0300

    🎉 TodoList project
    Start of TodoList App project.

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import Text from "./components/text"
+import Icon from "./components/icon"
+import TrashIcon from "./assets/icons/trash.svg?react"
+import CheckIcon from "./assets/icons/check.svg?react"
+import PlusIcon from "./assets/icons/plus.svg?react"
+import PencilIcon from "./assets/icons/pencil.svg?react"
+import SpinnerIcon from "./assets/icons/spinner.svg?react"
+import XIcon from "./assets/icons/x.svg?react"
+import Badge from "./components/badge"
+
+function App() {
+  
+  return (
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-1">
+        <Text variant="body-md">Item 1</Text>
+        <Text variant="body-md-bold"className="text-pink-base">Item 2</Text>
+        <Text variant="body-sm-bold" className="text-pink-dark">Item 3</Text>
+        <Text className="text-green-dark">Item para meu texto.</Text>
+      </div>
+
+      <div className="flex gap-1">
+          <Icon svg={TrashIcon} className="fill-green-base"></Icon>
+          <Icon svg={CheckIcon} />
+          <Icon svg={PlusIcon} />
+          <Icon svg={PencilIcon} />
+          <Icon svg={SpinnerIcon} animate />
+          <Icon svg={XIcon} />
+      </div>
+
+      <div>
+        <Badge variant="secondary"> 5 </Badge>
+        <Badge variant="primary">2 de 5</Badge>
+      </div>
+    </div>
+
+  )
+}
+
+export default App
